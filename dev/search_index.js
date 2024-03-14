873579b9ba92f56edc18c6335e71206de7d1fb19
Documenter.jl <documenter@juliadocs.github.io>
2024-03-14 13:15:41 +0000

    build based on e4b1cc7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GeometricIntegratorsBase","category":"page"},{"location":"#GeometricIntegratorsBase","page":"Home","title":"GeometricIntegratorsBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GeometricIntegratorsBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GeometricIntegratorsBase]","category":"page"}]
+}
